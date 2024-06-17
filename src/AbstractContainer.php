347c3ef165f3959611e3062221a7ce018b49affd
Jonathan Maron <jonathan.maron@theimagingsource.com>
2024-06-17 11:51:51 +0200

    Refactoring

--- src/AbstractContainer.php
+++ src/AbstractContainer.php
@@ -318,11 +318,6 @@ abstract class AbstractContainer implements Countable, RecursiveIterator
             if ($propertyValue === $value) {
                 return $page;
             }
-            /*
-            if ((string) $propertyValue === $value) {
-                return $page;
-            }
-            */
         }
 
         return null;
